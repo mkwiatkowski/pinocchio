cf3637e1fce7304f43ed65e244cb66564564c61f
Michal Kwiatkowski <michal@trivas.pl>
2014-11-12 16:56:52 +0100

    Bump version to 0.4.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pinocchio',
-    version="0.4.1",
+    version="0.4.2",
     description = 'pinocchio plugins for the nose testing framework',
     author = 'C. Titus Brown and Michal Kwiatkowski',
     author_email = 'titus@idyll.org,michal@trivas.pl',
